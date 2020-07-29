8ed8d6b9c9f626b245d22dcf6799260bf4789e21
manishmeshram51 <manishmeshram51@gmail.com>
2020-07-29 22:07:55 +0530

    andded logo and animation to it

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Login Page",
-      home: MyHomePage(),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  // Animation for flutter icon
+  AnimationController _iconAnimationController;
+  Animation<double> _iconAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _iconAnimationController = AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 300));
+
+    _iconAnimation = new CurvedAnimation(
+        parent: _iconAnimationController, curve: Curves.bounceOut);
+
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.blue,
       body: new Stack(
         fit: StackFit.expand,
         children: <Widget>[
@@ -35,7 +54,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           new Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[new FlutterLogo(size: 80.0)],
+            children: <Widget>[
+              new FlutterLogo(size: _iconAnimation.value * 100)
+            ],
           )
         ],
       ),
